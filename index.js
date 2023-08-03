89dfa7bb280baee7074ca024625fd823be4dffe4
Ubuntu <ubuntu@ip-172-31-16-251.us-west-1.compute.internal>
2023-08-03 02:39:32 +0000

    added ssl proper

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
+const https = require('https')
+const privateKey = fs.readFileSync( '/etc/letsencrypt/live/vanilla-sudoku.com/privkey.pem')
+const certificate = fs.readFileSync( '/etc/letsencrypt/live/vanilla-sudoku.com/cert.pem' )
+const port = 443
 
 app.use(express.static('static'))
 app.use('/static', express.static('static'))
 
 app.get('/', function (req, res) {
-	  res.send('Hello World')
+      res.send('Hello World')
 })
 
-app.listen(80)
+https.createServer({
+    key: privateKey,
+    cert: certificate,
+}, app).listen(port)

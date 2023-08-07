8c365cbcb07081ecc3721c5c5db2626b0bd625a7
Ubuntu <ubuntu@ip-172-31-16-251.us-west-1.compute.internal>
2023-08-07 22:02:30 +0000

    added basic logging

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const privateKey = fs.readFileSync( '/etc/letsencrypt/live/vanilla-sudoku.com/pr
 const certificate = fs.readFileSync( '/etc/letsencrypt/live/vanilla-sudoku.com/cert.pem' )
 const port = 443
 
+app.use((req, res, next) => { console.log(`${new Date().toUTCString()} ${req.method} ${req.path} ${req.ip}`); next() })
+app.use((error, req, res, next) => { console.log(`log error stuff ${req.path}`); next() })
 app.use(express.static('static'))
 app.use('/static', express.static('static'))
-
-app.get('/', (req, res) => {
-   res.send('Hello World')
-})
 
 app.all('*', (req, res) => {
     res.status(404).send("Not Found")
